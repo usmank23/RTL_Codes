aed15222c2bee028746a9fd44f5fecfde47fc419
usmank23 <124946371+usmank23@users.noreply.github.com>
2024-01-20 16:41:58 -0600

    Update parity_checker.v

--- Parity_Checker/parity_checker.v
+++ Parity_Checker/parity_checker.v
@@ -21,10 +21,10 @@
 
 
 module parity_checker(reset, load, rxin, dout, dout1, parityerror);
-input reset, rxin, load;  //this rxin is not the one coming from the transmitter
-input [7:0] dout;  //dout is coming from sipo module in UART
-output reg parityerror; // this is the main output that we want
-output [7:0] dout1; //This is one of the outputs generated from this module
+input reset, rxin, load;        //This rxin is not the one coming from the transmitter but something declared in testbench
+ input [7:0] dout;              //Dout is coming from sipo module in UART
+output reg parityerror;         // This is the main output that we want
+ output [7:0] dout1;            //This is one of the outputs generated from this module
 reg [3:0] a;
 wire [3:0] b;
 
